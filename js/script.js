e5c0d9d8c533bd0de68c25162f39bc5c1078ba48
Alexandr Krylov <alexandr.krylov@gmail.com>
2021-03-06 15:54:34 +0600

    ALL DONE!!!

--- js/script.js
+++ js/script.js
@@ -1,22 +1,53 @@
 (function(){
   /*save appointments*/
-  let i = 0;
+  // let i = 0;
+  let storage = window.localStorage;
+  let keyPrefix = "appointment";
   let button = document.querySelector('input[type="submit"]');
   button.onclick = function(){
-    i++;
+    // i++;
+    let recordName = getNextKey(storage, "appointment");
     let name = document.getElementsByName("name")[0].value;
     let surname = document.getElementsByName("surname")[0].value;
     let mobileNumber = document.getElementsByName("mobile-number")[0].value;
-    console.log(name, surname, mobileNumber);
-    // localStorage.setItem("appointment" + i, {})
-    alert("pressed " + i);
+    let problem =  (document.getElementsByName("problem")[0]).value;
+    let dateTime = document.querySelector('form [type="date"]').value + ' ' +
+    document.querySelector('form [type="time"]').value;
+    console.log(name, surname, mobileNumber, problem, dateTime);
+    console.log(recordName);
+    let storeData = JSON.stringify({ clientName: name,
+      surname: surname,
+      mobileNumber: mobileNumber,
+      problem: problem,
+      datetime: dateTime});
+    console.log(storeData);
+    storage.setItem(recordName, storeData);
+    console.log(storage.getItem(recordName));
+    saveCompleted(storage, keyPrefix);
     return false;
   }
-  console.log(button);
   // button.preventDefault();
   // if(ele.addEventListener){
-  //   ele.addEventListener("submit", callback, false);  //Modern browsers
-  // }else if(ele.attachEvent){
-  //   ele.attachEvent('onsubmit', callback);            //Old IE
-  // }
+  //   ele.addE
 })();
+
+function getNextKey(storage, prefix){
+  let i = 1;
+  while(storage.getItem(prefix + i)){
+    i++;
+  }
+  return prefix + i;
+}
+
+function saveCompleted(storage, prefix){
+  let  i = 1;
+  while(storage.getItem(prefix + i)){
+    i++;
+  }
+  if (storage.getItem(prefix + (i - 1))) {
+    alert("record saved. key = " + prefix + i + ", value = " + storage.getItem(prefix + (i - 1)) +
+      ". total records " + (i - 1));
+  } else {
+    alert("we have problems!!!");
+  }
+}
